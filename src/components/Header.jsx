a9025c8e02d16652960006574badbde2222bf63a
bierooed <hov.hovanes13@gmail.com>
2023-04-24 04:09:56 +0400

    Add styles for language switcher & change some attribute names

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,38 @@
 import React from "react";
 import paths from "../paths";
 import { setLang } from "../slices/languageSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import cn from "classnames";
 
 export default function Header() {
   const dispatch = useDispatch();
+  const currentLang = useSelector((state) => state.language.currentLang);
+
+  const getClassname = (lang) => {
+    return cn({
+      "hover:text-gray-600": true,
+      "text-black": currentLang === lang,
+      "text-gray-400": currentLang !== lang,
+    });
+  };
+
   return (
     <header className="flex md:justify-around xs: justify-between items-center pt-4 text-end">
       <div>
-        <button onClick={() => dispatch(setLang("ru"))}>ru</button> /{" "}
-        <button onClick={() => dispatch(setLang("en"))}>en</button>
+        <button
+          className={getClassname("ru")}
+          onClick={() => dispatch(setLang("ru"))}
+        >
+          ru
+        </button>{" "}
+        /{" "}
+        <button
+          className={getClassname("en")}
+          onClick={() => dispatch(setLang("en"))}
+        >
+          en
+        </button>
       </div>
       <div>
         <Link
@@ -26,9 +48,9 @@ export default function Header() {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             ></path>
           </svg>
         </Link>
