4cfa91897bc42ad7f05c3e44604e1de42a706a2a
bierooed <hov.hovanes13@gmail.com>
2023-04-25 02:58:20 +0400

    Create auth component and checks if the account logged in

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import AboutUs from "./AboutUs/AboutUs";
 import { Route, Routes } from "react-router-dom";
 import paths from "../paths";
+import Auth from "./Auth/Auth";
 
 export default function Main() {
   return (
     <main>
       <Routes>
         <Route path={paths.home} element={<AboutUs />} />
-        <Route path={paths.auth} element={<h1>Hello, world!</h1>} />
+        <Route path={paths.auth} element={<Auth />} />
       </Routes>
     </main>
   );
