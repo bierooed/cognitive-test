b5d089139b3f275265c0b9a2615fea467ed8ed42
bierooed <hov.hovanes13@gmail.com>
2023-05-04 22:42:24 +0400

    Keep firebase config info in .env

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "../../firebase";
+import auth from "../../firebase";
 
 export default function Auth() {
   const [isAuth, setIsAuth] = useState(false);
